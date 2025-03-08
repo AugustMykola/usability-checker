a452429f8c34e21e47699086206bc56757a68fcf
Mykola Nosachenko <19hagiri96@gmail.com>
2025-03-08 22:07:52 +0200

    minor

--- usability-checker/src/pages/first-experience-page/first-experience.tsx
+++ usability-checker/src/pages/first-experience-page/first-experience.tsx
@@ -1,16 +1,12 @@
 import {AppSidebar} from "@/components/side-menu/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar"
-import firstExperiencePageStyle from './first-experience.module.css'
 
-export default function FirstExperiencePage({ children }: { children: React.ReactNode }) {
+export default function FirstExperiencePage() {
     return (
         <div>
             <div className="relative">
                 <SidebarProvider defaultOpen={true}>
                     <AppSidebar />
-                    <div>
-                        {children}
-                    </div>
                 </SidebarProvider>
             </div>
         </div>
