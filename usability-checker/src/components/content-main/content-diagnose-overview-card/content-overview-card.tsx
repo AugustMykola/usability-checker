cd4d5cc39240af89b997a3bebbfd451932f5e910
Mykola Nosachenko <19hagiri96@gmail.com>
2025-03-09 20:12:42 +0200

    minor

--- usability-checker/src/components/content-main/content-diagnose-overview-card/content-overview-card.tsx
+++ usability-checker/src/components/content-main/content-diagnose-overview-card/content-overview-card.tsx
@@ -22,7 +22,7 @@ const ContentOverviewCard: React.FC<IContentOverviewCard> = ({title, score, stat
                 </div>
                 <Button className={overviewCardStyles.button}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                        <path d="M9.00526 6L18 6.00024V15M6 18L17.7111 6.28912" stroke="#0052FF" stroke-width="1.5"/>
+                        <path d="M9.00526 6L18 6.00024V15M6 18L17.7111 6.28912" stroke="#0052FF" strokeWidth="1.5"/>
                     </svg>
                 </Button>
             </div>
